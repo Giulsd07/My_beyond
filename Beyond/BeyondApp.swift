99b4fcf235f3deb476c8604405e0c371cfdb4438
Giulsd07 <dimeo.giulia.98@gmail.com>
2023-11-15 10:45:29 +0100

    Initial Commit

---
+++ Beyond/BeyondApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeyondApp.swift
+//  Beyond
+//
+//  Created by Giulia Di Meo on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BeyondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
